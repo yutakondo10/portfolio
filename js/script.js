cf7021840f4abcf55fa95bc6a9cda2ee5d3fda77
yuta <yuta.kondo1014@gmail.com>
2025-06-15 21:37:29 +0900

    worksからprojectへ変更、レイアウトと文章更新

--- js/script.js
+++ js/script.js
@@ -9,3 +9,34 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => response.text())
         .then(data => document.getElementById("footer").innerHTML = data);
 });
+// モーダル
+document.addEventListener("DOMContentLoaded", function () {
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modalImage");
+const openBtns = document.querySelectorAll(".modalOpen");
+
+// モーダルを開く
+openBtns.forEach(btn => {
+        btn.addEventListener("click", function (e) {
+            e.preventDefault();
+            const imgSrc = this.getAttribute("href");
+            modalImg.setAttribute("src", imgSrc);
+            modal.style.display = "flex";
+            document.body.classList.add("modal-open"); // スクロールを止める
+        });
+    });
+
+    modal.addEventListener("click", function (e) {
+        if (
+            e.target.classList.contains("modalClose") || 
+            e.target === modal
+        ) {
+            modal.style.display = "none";
+            modalImg.setAttribute("src", "");
+            document.body.classList.remove("modal-open"); // スクロールを戻す
+            setTimeout(() => {
+                modalImg.setAttribute("src", "");
+            }, 300); // CSSのアニメ時間に合わせる
+        }
+    });
+});
